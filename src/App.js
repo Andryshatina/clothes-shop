c9921d2f46227b58631f6e76e9bbc5d0ec0f3a01
Andrew <44811609+Andryshatina@users.noreply.github.com>
2021-12-03 18:26:03 +0200

    added header component with links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./components/shop/shop.component";
+import Header from "./components/header/header.component";
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 
@@ -9,6 +10,7 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
       <div>
+          <Header/>
           <Routes>
               <Route exact path='/' element={<HomePage/>}/>
               <Route path='/shop' element={<ShopPage/>}/>
